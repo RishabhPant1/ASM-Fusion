76468b1e84c2fe2e0922fee6032c88957cce1e2c
RishabhPant1 <139258559+RishabhPant1@users.noreply.github.com>
2025-06-21 21:44:20 +0530

    Delete converter.h

--- converter.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONVERTER_H
-#define CONVERTER_H
-#include "asm_fusion.h" // Include the shared header
-// Conversion function declaration
-string convertToC(const ParsedInstruction& instr);
-#endif // CONVERTER_H
